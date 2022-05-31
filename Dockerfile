a063fdee964243b089d0663486679da92bd4ffc5
Ragnar <tsivqdnil.rangar@gmail.com>
2022-05-31 23:04:02 +0300

    subseconds

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:3.16
 
+RUN apk add --update coreutils && rm -rf /var/cache/apk/*
+
 WORKDIR /usr/src/app
 
 COPY . .
--- hasher.sh
+++ hasher.sh
@@ -4,8 +4,8 @@ RND=`dd count=1 if=/dev/urandom 2>/dev/null | md5sum -b|cut -c1-32`
 
 while true; do
   #TS=`date -u --rfc-3339=ns`
-  #TS=`date -u -Ins`
-  TS=`date -u -R`
+  TS=`date -u -Ins`
+  #TS=`date -u -R`
   echo $TS $RND
   sleep 4.995;
 done
